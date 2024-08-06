00386da3208c6209c46e69aab14fe10ff69d3bb9
Bishnupriya Pradhan <86412967+Bishnupriyap01@users.noreply.github.com>
2024-08-06 17:38:40 +0530

    Update Customer.java

--- Activity2/src/main/java/com/bishnupriya/Activity2/Customer.java
+++ Activity2/src/main/java/com/bishnupriya/Activity2/Customer.java
@@ -12,3 +12,24 @@ public class Customer {
     private String lastName;
 
 }
+//alternative of above annotations!
+//     public Customer(String firstName, String lastName) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//     }
+
+//     public String getFirstName() {
+//         return firstName;
+//     }
+
+//     public void setFirstName(String firstName) {
+//         this.firstName = firstName;
+//     }
+
+//     public String getLastName() {
+//         return lastName;
+//     }
+
+//     public void setLastName(String lastName) {
+//         this.lastName = lastName;
+//     }
